9ace75011e215071aed3127bdf1a7f79f1f103b9
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-07-09 22:39:53 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/restImpl/UserRestImpl.java
@@ -0,0 +1,10 @@
+package com.example.demo.restImpl;
+
+import com.example.demo.rest.UserRest;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestImpl implements UserRest {
+
+
+}
