2023933b557f20174d2869b8b4b68a4da86820ac
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-07-13 22:29:11 +0530

    Delete UserDao.java

--- src/main/java/com/example/demo/repo/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import com.example.demo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface UserRepo extends JpaRepository<User,Integer> {
-    User findByEmail(@Param("email") String email);
-}
