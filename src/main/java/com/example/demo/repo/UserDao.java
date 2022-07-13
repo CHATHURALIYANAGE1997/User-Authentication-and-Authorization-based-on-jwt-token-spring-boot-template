dd1a7d522cb53b9ed16a3344428adbf375784670
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-07-13 22:23:32 +0530

    Update UserDao.java

--- src/main/java/com/example/demo/repo/UserDao.java
+++ src/main/java/com/example/demo/repo/UserDao.java
@@ -4,6 +4,6 @@ import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-public interface UserDao extends JpaRepository<User,Integer> {
+public interface UserRepo extends JpaRepository<User,Integer> {
     User findByEmail(@Param("email") String email);
 }
