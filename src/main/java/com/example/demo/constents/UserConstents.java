ad2cb3b84ff9d7fc5b9847d6ca7bb258e906ac06
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-08-12 22:20:07 +0530

    Update UserConstents.java

--- src/main/java/com/example/demo/constents/UserConstents.java
+++ src/main/java/com/example/demo/constents/UserConstents.java
@@ -1,1 +1,7 @@
+package com.example.demo.constents;
 
+
+public class UserConstents {
+    public static final String SOMETHIMG_WENT_WRONG="Something Went Wrong";
+    public  static  final  String ACCESS_DINAED="Acces Dinaed";
+}
