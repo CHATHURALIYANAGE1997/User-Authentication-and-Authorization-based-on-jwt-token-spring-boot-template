a46aa9d1b98a975aa04a7b2a64b1101e33b2cfa8
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-05 22:51:58 +0530

    Update UserRestImpl.java

--- src/main/java/com/example/demo/restImpl/UserRestImpl.java
+++ src/main/java/com/example/demo/restImpl/UserRestImpl.java
@@ -1,6 +1,11 @@
 package com.example.demo.restImpl;
 
+import com.example.demo.constents.UserConstents;
 import com.example.demo.rest.UserRest;
+import com.example.demo.service.UserService;
+import com.example.demo.utils.UserUntils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,15 +14,29 @@ import java.util.Map;
 
 @RestController
 public class UserRestImpl implements UserRest {
-
+    @Autowired
+    private UserService userService;
 
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
-        return null;
+        try {
+
+            return userService.signUp(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return UserUntils.getResponseEntity(UserConstents.SOMETHIMG_WENT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     @Override
     public ResponseEntity<String> login(Map<String, String> requestMap) {
-        return null;
+        try {
+
+            return userService.login(requestMap);
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return UserUntils.getResponseEntity(UserConstents.SOMETHIMG_WENT_WRONG,HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+
