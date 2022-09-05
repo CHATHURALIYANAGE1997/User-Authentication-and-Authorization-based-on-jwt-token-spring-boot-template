239c6c8f123ca817260426a3820f847174d405e6
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-05 18:59:36 +0530

    Update Config.java

---
+++ src/main/java/com/example/demo/JWT/Config.java
@@ -0,0 +1,12 @@
+package com.example.demo.JWT;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class Config extends WebSecurityConfigurerAdapter {
+    
+}
