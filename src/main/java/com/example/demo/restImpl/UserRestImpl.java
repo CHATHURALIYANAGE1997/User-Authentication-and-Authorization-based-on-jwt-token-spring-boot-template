a3402d2e8d5fce0b9d20b9224c0d2fa1f747cf10
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-05 20:17:55 +0530

    Update UserRestImpl.java

--- src/main/java/com/example/demo/restImpl/UserRestImpl.java
+++ src/main/java/com/example/demo/restImpl/UserRestImpl.java
@@ -1,10 +1,23 @@
 package com.example.demo.restImpl;
 
 import com.example.demo.rest.UserRest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 
 @RestController
 public class UserRestImpl implements UserRest {
 
 
+    @Override
+    public ResponseEntity<String> signUp(Map<String, String> requestMap) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> login(Map<String, String> requestMap) {
+        return null;
+    }
 }
