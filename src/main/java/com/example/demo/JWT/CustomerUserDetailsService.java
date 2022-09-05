e69bad37d04be05791fb8131b91f4c2c3d536d1f
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-05 19:02:26 +0530

    Update CustomerUserDetailsService.java

--- src/main/java/com/example/demo/JWT/CustomerUserDetailsService.java
+++ src/main/java/com/example/demo/JWT/CustomerUserDetailsService.java
@@ -1,1 +1,12 @@
+package com.example.demo.JWT;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+    
+}
