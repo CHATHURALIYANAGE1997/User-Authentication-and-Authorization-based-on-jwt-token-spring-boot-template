53db578c2d9d6294d85084c291f1637beff35174
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-05 19:13:53 +0530

    Update JwtFilter.java

---
+++ src/main/java/com/example/demo/JWT/JwtFilter.java
@@ -0,0 +1,10 @@
+package com.example.demo.JWT;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Component
+public class JwtFilter  extends OncePerRequestFilter {
+
+}
