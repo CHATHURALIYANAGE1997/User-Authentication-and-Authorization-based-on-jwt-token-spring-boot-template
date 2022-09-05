6e9b3b04649ad83a606e3870859cdde5049a7b2b
Chathura Liyanage <84259289+CHATHURALIYANAGE1997@users.noreply.github.com>
2022-09-05 20:17:05 +0530

    Update UserRest.java

--- src/main/java/com/example/demo/rest/UserRest.java
+++ src/main/java/com/example/demo/rest/UserRest.java
@@ -3,7 +3,7 @@ package com.example.demo.rest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path="/user")
